b15083177b80c0e8f7c0da2aa39e8bdf74ec6002
Lukas Brinkmann <lukas160394@web.de>
2022-08-29 22:07:01 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import MegaMenu from "primevue/megamenu";
+import { reactive } from "vue";
+
+interface NavigationItem {
+  label: string;
+  icon: string;
+  url: string;
+}
+
+const navigationItems = reactive<Array<NavigationItem>>([
+  { label: "Home", icon: "pi pi-fw pi-users", url: "/" },
+]);
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="container mx-auto">
+        <MegaMenu :model="navigationItems">
+          <template #item="{ item }">
+            <RouterLink :to="item.url">{{ item.label }}</RouterLink>
+          </template>
+        </MegaMenu>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import Table from "primevue/datatable";
+import Column from "primevue/column";
+import { reactive } from "vue";
+
+enum Period {
+  MONTHLY,
+  YEARLY,
+  DAILY,
+}
+
+enum Category {
+  TELECOMMUNICATION,
+  HOME,
+}
+
+interface Expense {
+  id: number;
+  title: string;
+  cost: number;
+  period: Period;
+  category: Category;
+}
+
+const expenses = reactive<Array<Expense>>([
+  {
+    id: 1,
+    title: "Telekom Mobilfunk",
+    cost: 24.99,
+    period: Period.MONTHLY,
+    category: Category.TELECOMMUNICATION,
+  },
+  {
+    id: 2,
+    title: "Telekom Festnetz",
+    cost: 54.99,
+    period: Period.MONTHLY,
+    category: Category.TELECOMMUNICATION,
+  },
+  {
+    id: 3,
+    title: "Miete (Kalt)",
+    cost: 300,
+    period: Period.MONTHLY,
+    category: Category.HOME,
+  },
+]);
+</script>
+
+<template>
+  <main class="container mx-auto">
+    <Table :value="expenses" responsiveLayout="scroll">
+      <template #empty> No customers found. </template>
+      <template #loading> Loading customers data. Please wait. </template>
+      <Column field="id" header="ID" />
+      <Column field="title" header="Ausgabe" />
+      <Column field="cost" header="Kosten" />
+      <Column field="period" header="Abrechnungsperiode" />
+    </Table>
+  </main>
+</template>
